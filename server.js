ac60fc2c89c2c76e78c0e3863f268a5c3c5b6e71
Yasuto Tanaka <yasuto@ymail.com>
2019-05-02 15:14:15 -0300

    fix compress order

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var path = require("path");
 var serveStatic = require("serve-static");
 
 var app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
 app.use(compression());
+app.use(serveStatic(path.join(__dirname, "dist")));
 
 var port = process.env.PORT || 5000;
 app.listen(port);
